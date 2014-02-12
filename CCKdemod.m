0447f4c942500e21f3ca555ee46bb08accf6d4ee
EDRappaport <ElliRappaport@gmail.com>
2014-02-12 16:02:46 -0500

    progress

--- CCKdemod.m
+++ CCKdemod.m
@@ -1,2 +1,13 @@
-function CCKdemod(RxMat)
+function Y = CCKdemod(RxMat)
 
+for ii=0:63
+    bin = de2bi(ii, 6, 'left-msb');
+    symbols(:,ii+1) = cckmod([0 0 bin]);
+end
+
+even_symbols = symbols;
+odd_symbols = -symbols;
+
+orig_phi1 = 0;
+
+Y=symbols;
