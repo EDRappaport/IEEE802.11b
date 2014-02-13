7e1f33e3fe7cb43accdfe3fb23aa4b0c1bd6d774
EDRappaport <ElliRappaport@gmail.com>
2014-02-13 17:02:01 -0500

    working using Euclidean distances, splitting real/imag

--- CCKdemod.m
+++ CCKdemod.m
@@ -19,44 +19,36 @@ for ii=0:63
     bin = de2bi(ii, 6, 'left-msb');
     symbols(:,ii+1) = cckmod([0 0 bin]);
 end
-symbolAng = angle(symbols);
-symbolAng(symbolAng < -3.141) = symbolAng(symbolAng < -3.141) + 2*pi;
-symbolAng(symbolAng > pi+.1) = symbolAng(symbolAng > pi+.1) - 2*pi;
+new_symbols = [real(symbols); imag(symbols)];
 
-prev_phi1 = 0; %initialize prev to 0
+prev_phi1 = exp(0); %initialize prev to 0
 
 BITS12 = [0 0; 0 1; 1 0; 1 1]; %possibilities for first 2 bits
 
 % go through input matrix, need for loop b/c phi1 is differential
 for ii=1:size(RxSymbMat, 1)
     
-    %find and isolate phi1
-    phi1 = angle(RxSymbMat(ii, 8));
-    phi1(phi1 < -3.141) = phi1(phi1 < -3.141) + 2*pi;
-    phi1(phi1 > pi+.1) = phi1(phi1 > pi+.1) - 2*pi;
-    RxAngles = angle(RxSymbMat(ii,:)) - phi1;
-    RxAngles(RxAngles < -3.141) = RxAngles(RxAngles < -3.141) + 2*pi;
-    RxAngles(RxAngles > pi+.1) = RxAngles(RxAngles > pi+.1) - 2*pi;
+    %isolate phi1
+    Rx_minus_phi1 = RxSymbMat(ii,:)*conj(RxSymbMat(ii, 8));
+    new_Rx_minus_phi1 = [real(Rx_minus_phi1) imag(Rx_minus_phi1)];
 
-    k(ii) = dsearchn(symbolAng.', RxAngles); %decimal # for last 6 bits +1
+    k(ii) = dsearchn(new_symbols.', new_Rx_minus_phi1); %decimal # for last 6 bits +1
     
     % find the differntial phi1, if odd symbol then extra pi shift
     if (mod(ii-1,2))
-        adjusted = phi1 - prev_phi1 - pi;
+        adjusted = RxSymbMat(ii, 8)*conj(prev_phi1)*exp(-pi*j);
     else
-        adjusted = phi1 - prev_phi1;
-    end 
-    adjusted(adjusted < 3.141) = adjusted(adjusted<3.141) + 2*pi;
-    adjusted(adjusted > pi+.1) = adjusted(adjusted > pi+.1) - 2*pi;
+        adjusted = RxSymbMat(ii, 8)*conj(prev_phi1);
+    end
     
     %find the phi1 offset and choose relevant
-    opt = dsearchn([0 pi/2 pi -pi/2].', adjusted);
+    opt = dsearchn([real([1 j -1  -j]); imag([1 j -1  -j])].', [real(adjusted) imag(adjusted)]);
     bits12 = BITS12(opt,:);
     add = bi2de([bits12 zeros(1,6)], 'left-msb'); % decimal # for first 2 bits
     
     k(ii) = k(ii)+add-1; %decimal # for 8 bits
     
-    prev_phi1 = phi1;
+    prev_phi1 = RxSymbMat(ii, 8);
     
 end
 
