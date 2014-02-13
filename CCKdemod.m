95c57fbafe75b39459b26e26814ac5e6185a7fa4
EDRappaport <ElliRappaport@gmail.com>
2014-02-13 00:21:32 -0500

    maybe working

--- CCKdemod.m
+++ CCKdemod.m
@@ -5,9 +5,20 @@ for ii=0:63
     symbols(:,ii+1) = cckmod([0 0 bin]);
 end
 
-even_symbols = symbols;
-odd_symbols = -symbols;
+even_symbols = angle(symbols);
+odd_symbols = angle(-symbols);
 
-orig_phi1 = 0;
+prev_phi1 = 0;
+
+for ii=1:size(RxMat, 1)
+    if (mod(ii-1, 2))
+        prev_phi1 = angle(-RxMat(ii,8)) - prev_phi1;
+    else
+        prev_phi1 = angle(RxMat(ii,8)) - prev_phi1;
+    end
+    RxAngles = angle(RxMat(ii,:)) - prev_phi1;
+    k = dsearchn(even_symbols.', RxAngles)   
+    
+end
 
 Y=symbols;
