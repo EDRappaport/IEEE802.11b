377c0fc6771ad25f4e3b263290656ea821e023c7
EDRappaport <ElliRappaport@gmail.com>
2014-02-13 10:09:57 -0500

    fixed 1 bug!!(-pi==pi)

--- CCKdemod.m
+++ CCKdemod.m
@@ -1,9 +1,10 @@
-function Y = CCKdemod(RxMat)
+function Y = CCKdemod( RxMat )
 
 for ii=0:63
     bin = de2bi(ii, 6, 'left-msb');
     symbols(:,ii+1) = cckmod([0 0 bin]);
 end
+symbols(symbols==-pi) = pi;
 
 even_symbols = angle(symbols);
 odd_symbols = angle(-symbols);
@@ -11,14 +12,22 @@ odd_symbols = angle(-symbols);
 prev_phi1 = 0;
 
 for ii=1:size(RxMat, 1)
-    if (mod(ii-1, 2))
-        prev_phi1 = angle(-RxMat(ii,8)) - prev_phi1;
-    else
-        prev_phi1 = angle(RxMat(ii,8)) - prev_phi1;
-    end
+%     if (mod(ii-1, 2))
+%         prev_phi1 = angle(-RxMat(ii,8)) - prev_phi1
+%     else
+%         prev_phi1 = angle(RxMat(ii,8)) - prev_phi1
+%     end
+    prev_phi1 = angle(RxMat(ii, 8));
+    prev_phi1(prev_phi1==-pi) = pi;
     RxAngles = angle(RxMat(ii,:)) - prev_phi1;
-    k = dsearchn(even_symbols.', RxAngles)   
+    RxAngles(RxAngles==-pi) = pi;
+    if (mod(ii-1, 2))
+        k(ii) = dsearchn(odd_symbols.', RxAngles);
+    else
+        k(ii) = dsearchn(even_symbols.', RxAngles);
+    end
     
 end
 
-Y=symbols;
+%Y = de2bi(k-1, 8, 'left-msb');
+Y=k;
