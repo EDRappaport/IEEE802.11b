f4c3bc0b55dd0d876f640147e3999a4eb5a1e38a
Elie_Weintraub <elieweintraub@gmail.com>
2014-02-18 16:22:48 -0500

    ARF script and new script that lets you select to use ARF or fixed data rate

--- IEEE80211b_script.m
+++ IEEE80211b_script.m
@@ -5,13 +5,17 @@
 % Weintraub, Elie
 % Weintraub, Hillel
 %
-% IEEE80211b_ARF_script.m: Implements ARF for IEEE802.11b
+% IEEE80211b_script.m: Implements IEEE802.11b either at a fixed data rate
+% or employing ARF.
 
-clc, clear all, close all;
+clc,clear all,close all;
 
-%% ARF Parameters
-ERR_THRESH = PacketSizeBits/100; % errors above which packet is bad
-SUCCESS_THRESH = 5;              % num of good Txs after which we inc. rate
+%% Mode Selection 
+ARF = input('Do you want to use ARF: (true, false)');
+if ~ARF
+    RATE = input(['What fixed data rate do you want to use:'...
+                  '(1:1Mbps,2:2 Mbps,3:5.5Mbps,4: 11Mbps)']);
+end
 
 %% Simulation Parameters
 EbNo = 0:2:10;                   % range of noise levels 
@@ -27,20 +31,26 @@ BitsPerSymbols = [1, 2, 4, 8];
 SpreadingRates = [11, 11, 8, 8];
 calcSnr = @(rate,EbNo) EbNo +10*log10(BitsPerSymbols(rate))...
                             -10*log10(SpreadingRates(rate)*SamplesPerChip);
-
+                        
+%% ARF Parameters
+if ARF
+    ERR_THRESH = PacketSizeBits/100; % errors above which packet is bad
+    SUCCESS_THRESH = 5;              % num of good Txs after which we inc. rate
+    RateMat=zeros(NumPackets,length(EbNo));
+end
 
 %% Main BER Loop
-RateMat=zeros(NumPackets,length(EbNo));
 BER = zeros(length(EbNo),1);
 for i=1:length(EbNo)
     
     %Initialize Stats
-    rate = 1;  TotalBits = 0; ErrorBits = 0;
-    numSuccess = 0; numFail = 0; probe = false; 
-  
+    if ARF
+        rate = 1;  TotalBits = 0; ErrorBits = 0;
+        numSuccess = 0; numFail = 0; probe = false; 
+    else
+        rate = RATE;  TotalBits = 0; ErrorBits = 0;
+    end
     for packet = 1:NumPackets
-        
-        RateMat(packet,i)=rate;
         
         snr = calcSnr(rate,EbNo(i));
         
@@ -60,17 +70,21 @@ for i=1:length(EbNo)
         ErrorBits = ErrorBits + NewErrorBits;
         
         %Adjust ARF parameters (numSuccess, numFail, pilot, rate)
-        if NewErrorBits > ERR_THRESH   % failure
-            numFail = numFail+1;
-            if (numFail == 2 || (numFail == 1 && probe)) && rate > 1
-                rate = rate-1; numSuccess = 0; numFail = 0;  probe = false;
-            end
-        else                          % success
-            numSuccess = numSuccess+1;
-            if numSuccess == SUCCESS_THRESH && rate < 4
-                rate = rate+1; numSuccess = 0; numFail = 0; probe = true;
-            else
-                probe = false; numFail = 0;
+        if ARF
+            RateMat(packet,i)=rate;
+             
+            if NewErrorBits > ERR_THRESH   % failure
+                numFail = numFail+1;
+                if (numFail == 2 || (numFail == 1 && probe)) && rate > 1
+                    rate = rate-1; numSuccess = 0; numFail = 0;  probe = false;
+                end
+            else                          % success
+                numSuccess = numSuccess+1;
+                if numSuccess == SUCCESS_THRESH && rate < 4
+                    rate = rate+1; numSuccess = 0; numFail = 0; probe = true;
+                else
+                    probe = false; numFail = 0;
+                end
             end
         end
         
