5b547d9f9bf7218a11030cd24945d4039530ac08
EDRappaport <ElliRappaport@gmail.com>
2014-02-19 11:01:27 -0500

    changing EbNo

--- IEEE80211b_ARF.m
+++ IEEE80211b_ARF.m
@@ -1,43 +1,99 @@
-clear all;
+% ECE408: Wireless Communications
+% Implementation of PHY layer of IEEE802.11b
+%
+% Rappaport, Elliot D
+% Weintraub, Elie
+% Weintraub, Hillel
+%
+% IEEE80211b_ARF.m: Implements IEEE802.11b using ARF and a changing EbNo.
+clear all,close all;
 
+tic
 %% Simulation Parameters
-EbNo = 0:10;                    % range of noise levels 
-NumPackets = 1e3;               % number of packets sent
-PacketSizeBits = 8192;          % 802.11 packet size
+NumPackets = 200;                 % number of packets sent
+EbNo = 10*(1+sin([1:NumPackets]/10));% range of noise levels 
+PacketSizeBits = 8192;           % 802.11 packet size
+SamplesPerChip = 8;       
 
-modulateFunctions = {@(x) barker1mod(x), @(x) barker2mod(x),...
+%% ARF Parameters
+ERR_THRESH = PacketSizeBits/100; % num errors above which packet is bad
+SUCCESS_THRESH = 5;              % num of good pkts needed to transition
+RateMat=zeros(NumPackets,1);
+
+%% Setup
+modulateFunctions = {@(x) barkermod(x, 1), @(x) barkermod(x, 2),...
     @(x) cckmod(x, 4), @(x) cckmod(x, 8)};
-demodulateFunctions = {@(x) barker1demod(x), @(x) barker2demod(x),...
-    @(x) CCKdemod(x, 4), @(x) CCKdemod(x, 8)};
-
+demodulateFunctions = {@(x,y) barkerdemod(x, 1), @(x,y) barkerdemod(x, 2),...
+    @(x,y) CCKdemod(x, 4, y), @(x,y) CCKdemod(x, 8, y)};
+BitsPerSymbols = [1, 2, 4, 8];
+SpreadingRates = [11, 11, 8, 8];
+DataRates = [1, 2, 5.5, 11];
+calcSnr = @(rate,EbNo) EbNo +10*log10(BitsPerSymbols(rate))...
+                            -10*log10(SpreadingRates(rate)*SamplesPerChip);
+                        
 %% Main BER Loop
-for i=1:length(EbNo)
+BER = zeros(length(EbNo),1);
+
+%Initialize Stats
+rate = 1;  TotalBits = 0; ErrorBits = 0;
+numSuccess = 0; numFail = 0; probe = false; 
+
+for packet = 1:NumPackets
+
+    disp(['Simulating EbNo = ' num2str(EbNo(i)),...
+          ' Packet ' num2str(packet) '...']);
+
+    snr = calcSnr(rate,EbNo(packet));   %EbNo always changing
+
+    %Transmitter
+    TxBits = randi([0 1],PacketSizeBits,1);
+    TxChips = modulateFunctions{rate}(TxBits);
+    [Samples,h,FilterDelayInChips] = TxFilter(TxChips,SamplesPerChip);
     
-    %Initialize Stats
-    numSuccess = 0;
-    numFail = 0;
-    rate = 1;
-    for packet = 1:NumPackets
-        
-        if numSuccess > 9
-            rate = min(rate+1,4);
-        elseif numFail > 1
-            rate = max(rate-1, 0);
+    ChannelOutput = awgn(Samples, snr, 'measured'); 
+    
+    %Receiver
+    [RxChips,TotalDelayInBits] = RxFilter(ChannelOutput,h,...
+                              SamplesPerChip,FilterDelayInChips,...
+                            BitsPerSymbols(rate),SpreadingRates(rate));
+    RxBits = demodulateFunctions{rate}(RxChips, TotalDelayInBits);
+
+    %Calculate error 
+    TotalBits = TotalBits+length(RxBits)-TotalDelayInBits;
+    NewErrorBits = sum(TxBits(1:end-TotalDelayInBits)~=...
+                    RxBits(TotalDelayInBits+1:end));
+    ErrorBits = ErrorBits + NewErrorBits;
+
+    %Adjust ARF parameters (numSuccess, numFail, pilot, rate)
+    RateMat(packet)=rate;
+
+    if NewErrorBits > ERR_THRESH   % failure
+        numFail = numFail+1;
+        if (numFail == 2 || (numFail == 1 && probe)) && rate > 1
+            rate = rate-1; numSuccess = 0; numFail = 0;probe = false;
         end
-        
-        snr = calcSnr{rate}(EbNo(i));
-        
-        TxBits = randi([0 1],PacketSizeBits,1);
-        TxChips = modulateFunctions{rate}(TxBits);
-        TxSamplesFiltered = ourTxFilter(TxChips);
-        ChannelOutput = awgn(TxSamplesFiltered, snr, 'measured');
-        RxChips = ourRxFilter(ChannelOutput);
-        
-        RxBits = demodulateFunctions{rate}();
-        
-        %claculate error and adjust numSuccess and numFail
-        
+    else                          % success
+        numSuccess = numSuccess+1;
+        if numSuccess == SUCCESS_THRESH && rate < 4
+            rate = rate+1; numSuccess = 0; numFail = 0; probe = true;
+        else
+            probe = false; numFail = 0;
+        end
     end
+
 end
-        
-        
+
+%% Plot ARF transitions
+h = figure('Name','ARF Transitions'); hold on;
+subplot(211); plot(1:NumPackets,RateMat);
+xlabel('Packet Number');ylabel('Data Rate');
+xlim([1 NumPackets]);ylim([0 5]);title('ARF Transitions');   
+set(gca,'yTickLabel',{'','1Mbps','2Mbps','5.5Mbps','11Mbps',''});
+
+subplot(212); plot(1:NumPackets, EbNo);
+xlabel('Packet Number');ylabel('EbNo');
+
+% saveas(h, 'Transitions.fig');
+% saveas(h, 'Transitions.png');
+
+toc
